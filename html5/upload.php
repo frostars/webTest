5e1e2c22311167b7a775acbe9678956d6e57215e
陈星星 <102@chinadance.com>
2015-11-19 15:07:54 +0800

    html5实现拖拽上传

---
+++ html5/upload.php
@@ -0,0 +1,10 @@
+﻿<?php 
+if(!empty($_FILES['form'])){
+	move_uploaded_file($FILES['form']['tmp_name'],$FILES['form']['name']);
+}
+?>
+<meta charset="utf-8" />
+<form action="" method="post" enctype="multipart/form-data">
+	<input type="file" name="ff" />
+	<input type="submit" value="上传" />
+</form>
